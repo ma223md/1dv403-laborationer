031a98de4289e01ed3607f1f19ffd6ab77b685fa
Moa Alfredsson <ma223md@student.lnu.se>
2014-11-05 17:51:43 +0100

    Completed the function "guess"

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,21 +2,36 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
+    var secret = Math.floor((Math.random() * 100) + 1); // Slumpat tal
+    var guesses = 0; // Antal gissningar
 	
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var guess = function(number){
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
 			
-		// Plats för förändring.
+		guesses += 1 // Räknar antalet gissningar
 
-
-		// Returnera exempelvis: 
-		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
-		// [false, "Det hemliga talet är högre!"]
-		// [false, "Det hemliga talet är lägre!"]
-		// [false, "Talet är utanför intervallet 0 - 100"]		
+	    // Om secret är samma som gissningen
+		if (number > 100 || number < 1) {
+		    return [false, "Talet är utanför intervallet 0 - 100"]
+		}
+	    // Om secret inte är ett tal
+		if (isNaN(number) == true) {
+		    return [false, "Du måste ange ett tal"]
+		}
+	    // Om gissningen är korrekt
+		if (secret == number){
+		    return [true, "Grattis du vann! Det hemliga talet var " + secret + " och du behövde "+ guesses + " gissningar för att hitta det."]
+		}
+		// Om gissning är högre än secretNumber
+		else if (secret < number){
+		    return [false, "Det hemliga talet är lägre!"]
+		}
+		// Om gissning är lägre än secretNumber
+		else if (secret > number) {
+		    return [false, "Det hemliga talet är högre!"]
+		}
 	};
 	
 	// ------------------------------------------------------------------------------
