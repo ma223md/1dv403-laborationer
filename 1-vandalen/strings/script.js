fd46d9d562fe4c90a44f40f8743567fd686e1e31
Moa Alfredsson <ma223md@student.lnu.se>
2014-11-06 18:17:11 +0100

    skeleton for the sting assignment, array undefined bug.

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -3,16 +3,62 @@
 window.onload = function(){
 
 	// I denna funktion ska du skriva koden för att hantera "spelet"
-	var convertString = function(str){
-		// Plats för förändring.		
-		// Returnera den konverterade strängen.
-		// Vid fel, kasta ett undantag med ett meddelande till användaren. 
-	
+    var convertString = function(str){
+        // Plats för förändring.
+
+        // Gör om sträng till array med bokstäver
+        var stingArray = str.split("");
+
+        function upperToLower(char) {
+            return char.toLowerCase();
+        }
+
+        function lowerToUpper(char) {
+            return char.toUpperCase();
+        }
+
+        for (var i = 0; i < stingArray.length; i++) {
+            // if uppercase kalla på uppertoLower
+            // else if lowercase kalla på lowertoUpper
+            // if a or A kovertera till #
+
+        }
+
+        return str;
 
 
+        //// Gå igenom array bokstav för bokstav
+        //for (var i = 0; i < stingArray.length; i++){
+        //        if (stringArray[i] != stringArray[i].toUpperCase()) {
+        //                // Om bokstav är lowercase konvertera till uppercase
+        //                stringArray[i] = stringArray[i].toUpperCase();
+        //            }
+        //        else {
+        //                // Om bokstav är uppercase konvertera till lowercase
+        //                stringArray[i] = stringArray[i].toLowerCase();
+        //        }
+        //        if (stringArray[i] == "a" || stringArray[i] == "A") {
+        //            // Om bokstav är A eller a konvertera till #
+        //            stringArray[i] = "#";
+        //        }     
+        //    }
+        //// Sätt ihop array till sträng
+        ////str = stingArray.join("");
+        //// Returnera konverterad sträng
+        //return str;
 
+        // Vid fel, kasta ett undantag med ett meddelande till användaren.
 
+        //var convertedString = '';
+        //var convertedString = str.toUpperCase();
 
+        //for (var i = 0; i < stingArray.length; i++) {
+        //    if (stingArray.charCodeAt(i) >= 65 && stingArray.charCodeAt(i) <= 90) {
+        //        convertedString = convertedString + stingArray.charAt(i).toLowerCase();
+        //    }
+        //    else
+        //        convertedString = convertedString + stingArray.charAt(i).toUpperCase();
+        //}
 
 	};
 	// ------------------------------------------------------------------------------
