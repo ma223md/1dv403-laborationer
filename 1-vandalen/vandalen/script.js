52df8f7c4d152218098d5c174bfaea219686ef24
Moa Alfredsson <ma223md@student.lnu.se>
2014-11-11 14:44:57 +0100

    skeleton for lab 1.4

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,8 +2,19 @@
 
 var makePerson = function(persArr){
 
-
-	// Din kod här...
+    // dela upp array till namn och åldrar
+    for (var i = 0; i < persArr.length; i++) {
+        // dela sträng till namn och ålder
+        var split = persArr[i].split(" ")
+        var name = split[1];
+        var age = split[4];
+        // stoppa namn och ålder i varsin array
+        var nameArray = [name];
+        var ageArray = [age];
+    }
+    // sortera åldrar
+    ageArray.sort
+    // sortera namn
 
 }
 
