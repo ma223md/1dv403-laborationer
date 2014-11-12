7de10a70a8e59355995331317df2f327532c9d63
Moa Alfredsson <ma223md@student.lnu.se>
2014-11-13 00:13:12 +0100

    tests 1 & 2 working

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,19 +2,42 @@
 
 var makePerson = function(persArr){
 
-    // dela upp array till namn och åldrar
-    for (var i = 0; i < persArr.length; i++) {
-        // dela sträng till namn och ålder
-        var split = persArr[i].split(" ")
-        var name = split[1];
-        var age = split[4];
-        // stoppa namn och ålder i varsin array
-        var nameArray = [name];
-        var ageArray = [age];
-    }
-    // sortera åldrar
-    ageArray.sort
-    // sortera namn
+    var result = {};
+    // Lös uppgiften
+    var nameArray = [];
+    var ageArray = [];
+    var nameString = "";
 
+    // loopa igenom person-array
+    for (var i = 0; i < persArr.length; i++) {
+        // stoppa namn och ålder i varsin array
+        //nameArray.push(persArr[i].name);
+        //ageArray.push(persArr[i].age);
+        nameArray[i] = persArr[i].name;
+        ageArray[i] = persArr[i].born;
+    }
+
+    // sortera åldrar
+    ageArray.sort(function (a, b) { return a - b });
+
+    // sortera namn
+    nameArray.sort();
+
+    //sätt ihop till sträng
+    
+    for (i = 0; i < nameArray.length; i++) {
+        nameString += nameArray[i];
+        // lägg till "," till alla utom sista namnet
+        nameString += (nameArray.length - 1 === i) ? "" : ", ";
+    }
+
+    // egenskaper för retur-objekt
+    result.maxAge = ageArray[ageArray.length];
+    result.minAge = ageArray[0];
+    result.averageAge = 13;
+    result.names = nameString;
+
+    return result;
+    //var data = [{ name: "John Häggerud", age: 37 }, { name: "Johan Leitet", age: 36 }, { name: "Mats Loock", age: 46 }];
 }
 
