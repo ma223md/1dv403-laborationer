93059e28e1c731fce74d37390dd0fa80683bfccf
Moa Alfredsson <moa.alfredsson@gmail.com>
2014-11-25 16:11:47 +0000

    Moment 1 & 2

---
+++ 2-labbymezzage/js/message.js
@@ -0,0 +1,30 @@
+function Messsage(message, date) {
+    
+    this.getText = function(){
+        return message;
+    }
+    
+    this.setText = function(_text) {
+        message = text;
+    }
+    
+    this.getDate = function(){
+        return date;
+    }
+    
+    this.setDate = function(_date) {
+       date = _date 
+    }
+    
+    Message.prototype.toString = function() {
+        return this.getText() + " (" + this.getDate() + ")"
+    }
+    
+    Message.prototype.getHTMLText = function() {
+        return this.getText();
+    }
+    
+    Message.prototype.getDateText = function() {
+        return this.getDate().toLocalTimeString();
+    }
+}
