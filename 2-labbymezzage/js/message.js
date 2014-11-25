06a384e825fc4089c9f3a4502dc088eabd4bebc0
Moa Alfredsson <moa.alfredsson@gmail.com>
2014-11-25 20:24:31 +0000

    load event working

--- 2-labbymezzage/js/message.js
+++ 2-labbymezzage/js/message.js
@@ -1,30 +1,33 @@
-function Messsage(message, date) {
+"use strict";
+window.onload = function(){
+    function Messsage(message, date) {
     
-    this.getText = function(){
-        return message;
-    }
-    
-    this.setText = function(_text) {
-        message = text;
-    }
-    
-    this.getDate = function(){
-        return date;
-    }
-    
-    this.setDate = function(_date) {
-       date = _date 
-    }
-    
-    Message.prototype.toString = function() {
-        return this.getText() + " (" + this.getDate() + ")"
-    }
-    
-    Message.prototype.getHTMLText = function() {
-        return this.getText();
-    }
-    
-    Message.prototype.getDateText = function() {
-        return this.getDate().toLocalTimeString();
+        this.getText = function(){
+            return message;
+        }
+        
+        this.setText = function(_text) {
+            message = text;
+        }
+        
+        this.getDate = function(){
+            return date;
+        }
+        
+        this.setDate = function(_date) {
+           date = _date 
+        }
+        
+        Message.prototype.toString = function() {
+            return this.getText() + " (" + this.getDate() + ")"
+        }
+        
+        Message.prototype.getHTMLText = function() {
+            return this.getText();
+        }
+        
+        Message.prototype.getDateText = function() {
+            return this.getDate().toLocalTimeString();
+        }
     }
 }
