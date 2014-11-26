1a0df11d73f96dddd0254bf1e7d25160ed980f12
Moa Alfredsson <moa.alfredsson@gmail.com>
2014-11-26 12:15:17 +0000

    Finished step 2, object instancing working

--- 2-labbymezzage/js/message.js
+++ 2-labbymezzage/js/message.js
@@ -1,33 +1,32 @@
 "use strict";
-window.onload = function(){
-    function Messsage(message, date) {
+
+function Message(message, date) {
     
-        this.getText = function(){
-            return message;
-        }
-        
-        this.setText = function(_text) {
-            message = text;
-        }
-        
-        this.getDate = function(){
-            return date;
-        }
-        
-        this.setDate = function(_date) {
-           date = _date 
-        }
-        
-        Message.prototype.toString = function() {
-            return this.getText() + " (" + this.getDate() + ")"
-        }
-        
-        Message.prototype.getHTMLText = function() {
-            return this.getText();
-        }
-        
-        Message.prototype.getDateText = function() {
-            return this.getDate().toLocalTimeString();
-        }
+    this.getText = function(){
+        return message;
+    }
+    
+    this.setText = function(_text) {
+        message = _text;
+    }
+    
+    this.getDate = function(){
+        return date;
+    }
+    
+    this.setDate = function(_date) {
+       date = _date 
+    }
+    
+    Message.prototype.toString = function() {
+        return this.getText() + " (" + this.getDate() + ")"
+    }
+    
+    Message.prototype.getHTMLText = function() {
+        return this.getText();
+    }
+    
+    Message.prototype.getDateText = function() {
+        return this.getDate().toLocalTimeString();
     }
 }
