40f5b4f5155f810cef4c68eecd3bd040bdb228e1
Moa Alfredsson <moa.alfredsson@gmail.com>
2014-12-01 13:29:48 +0000

    Creating elements for each brick

--- 3-gameon/memory/js/memory.js
+++ 3-gameon/memory/js/memory.js
@@ -8,15 +8,37 @@ var Memory = {
     
     // function for new game
     newGame: function(){
-        // randomizedArray = randomized();
         var memorybricks = RandomGenerator.getPictureArray(4,4);
         
-        // create new elements for game
+        // create new elements to test randomized array
         var div = document.getElementById("memoryboard"),
         text = document.createElement("p");
         
         text.innerHTML = Memory.memorybricks;
         div.appendChild(text);
+        
+        // create table for memoryboard
+        var memorytable = document.createElement("table")
+        div.appendChild(memorytable);
+        
+        // add each arrayitem to table
+        for (var i = 0; i < memorybricks.length; i++){
+            //call function for creating brick
+            newBrick(i);
+        }
+        
+        //function for creating bricks
+        newBrick = function(i){
+            var brickgrid = document.createElement("td"),
+            link = document.createElement("a"),
+            picture = document.createElement("img");
+            
+            memorytable.appendChild(brickgrid);
+            brickgrid.appendChild(link);
+            link.appendChild(picture);
+            
+            // onclick funtion goes here
+        }
     },
 };
 
