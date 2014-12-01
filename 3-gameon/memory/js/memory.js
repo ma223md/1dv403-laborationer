38c18cffcf868c2bfcc84cd9caa04a4f4767ce55
Moa Alfredsson <moa.alfredsson@gmail.com>
2014-12-01 12:46:10 +0000

    css fonts working

--- 3-gameon/memory/js/memory.js
+++ 3-gameon/memory/js/memory.js
@@ -4,19 +4,20 @@ var Memory = {
     init: function(){
         // create a memory game, call function
         Memory.newGame();
-    }
-    
-    // function for creating randomized array
-    randomized = function(){
-        var memorybricks = RandomGenerator.getPictureArray(4,4);
-    }
+    },
     
     // function for new game
-    newGame = function(){
-        randomizedArray = randomized();
+    newGame: function(){
+        // randomizedArray = randomized();
+        var memorybricks = RandomGenerator.getPictureArray(4,4);
         
         // create new elements for game
-    };
-}
+        var div = document.getElementById("memoryboard"),
+        text = document.createElement("p");
+        
+        text.innerHTML = Memory.memorybricks;
+        div.appendChild(text);
+    },
+};
 
 window.onload = Memory.init;  //init run when site is loaded
