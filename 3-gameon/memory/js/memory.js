dba83a840424d7a24de1644fea6baa738cc81688
Moa Alfredsson <moa.alfredsson@gmail.com>
2014-12-01 12:23:21 +0000

    added array and function for creating new game

--- 3-gameon/memory/js/memory.js
+++ 3-gameon/memory/js/memory.js
@@ -2,9 +2,21 @@
 
 var Memory = {
     init: function(){
-        alert("Hejhopp");
+        // create a memory game, call function
+        Memory.newGame();
     }
     
+    // function for creating randomized array
+    randomized = function(){
+        var memorybricks = RandomGenerator.getPictureArray(4,4);
+    }
+    
+    // function for new game
+    newGame = function(){
+        randomizedArray = randomized();
+        
+        // create new elements for game
+    };
 }
 
 window.onload = Memory.init;  //init run when site is loaded
