1aaa27e55432277d836035e7c5684903c11d64cb
Moa Alfredsson <moa.alfredsson@gmail.com>
2014-12-02 13:53:40 +0000

    trying out the new pictures for the bricks

--- 3-gameon/memory/js/memory.js
+++ 3-gameon/memory/js/memory.js
@@ -1,45 +1,48 @@
 "use strict"
 
-var Memory = {
+function Memory(newBoard){
     init: function(){
         // create a memory game, call function
         Memory.newGame();
-    },
+    }
     
     // function for new game
     newGame: function(){
-        var memorybricks = RandomGenerator.getPictureArray(4,4);
+        var bricks = RandomGenerator.getPictureArray(5,5);
         
         // create new elements to test randomized array
         var div = document.getElementById("memoryboard"),
         text = document.createElement("p");
         
-        text.innerHTML = Memory.memorybricks;
+        text.innerHTML = Memory.memorybricks[1];
         div.appendChild(text);
         
         // create table for memoryboard
         var memorytable = document.createElement("table")
         div.appendChild(memorytable);
         
+
         // add each arrayitem to table
         for (var i = 0; i < memorybricks.length; i++){
             //call function for creating brick
             newBrick(i);
         }
-        
+                
         //function for creating bricks
-        newBrick = function(i){
+        var newBrick = function(i){
             var brickgrid = document.createElement("td"),
             link = document.createElement("a"),
             picture = document.createElement("img");
             
             memorytable.appendChild(brickgrid);
             brickgrid.appendChild(link);
+            brickgrid.appendChild(memorybricks[i])
             link.appendChild(picture);
             
             // onclick funtion goes here
         }
-    },
+
+    }
 };
 
 window.onload = Memory.init;  //init run when site is loaded
