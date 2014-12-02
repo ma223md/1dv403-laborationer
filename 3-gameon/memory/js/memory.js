07500fee2a617806eb4b1d9991b468f51e75c2b4
Moa Alfredsson <moa.alfredsson@gmail.com>
2014-12-02 15:33:20 +0000

    writes out all of the bricks to the div

--- 3-gameon/memory/js/memory.js
+++ 3-gameon/memory/js/memory.js
@@ -1,48 +1,57 @@
 "use strict"
 
-function Memory(newBoard){
+var Memory = {
+    // define variables
+    memoryBricks: [],
+    
     init: function(){
         // create a memory game, call function
         Memory.newGame();
-    }
+    },
     
     // function for new game
     newGame: function(){
-        var bricks = RandomGenerator.getPictureArray(5,5);
-        
+        Memory.memoryBricks = RandomGenerator.getPictureArray(4,4);
         // create new elements to test randomized array
-        var div = document.getElementById("memoryboard"),
-        text = document.createElement("p");
-        
-        text.innerHTML = Memory.memorybricks[1];
-        div.appendChild(text);
+        var div = document.getElementById("memoryboard");
         
         // create table for memoryboard
-        var memorytable = document.createElement("table")
-        div.appendChild(memorytable);
+        // var memorytable = document.createElement("table")
+        // div.appendChild(memorytable);
         
 
         // add each arrayitem to table
-        for (var i = 0; i < memorybricks.length; i++){
+        for (var i = 0; i < Memory.memoryBricks.length; i++){
+            // create new link and image
+            var link = document.createElement("a"),
+            image = document.createElement("img"),
+            arrayTest = Memory.memoryBricks[i];
+            
+            link.setAttribute("class", "memorybrick");
+            
+            div.appendChild(link);
+            link.appendChild(image);
+            link.innerHTML = Memory.memoryBricks[i];
+            // onclick funtion goes here
             //call function for creating brick
-            newBrick(i);
+            //newBrick(i);
         }
                 
         //function for creating bricks
         var newBrick = function(i){
-            var brickgrid = document.createElement("td"),
-            link = document.createElement("a"),
-            picture = document.createElement("img");
+            // create new link and image
+            var link = document.createElement("a"),
+            image = document.createElement("img"),
+            arrayTest = Memory.memoryBricks[i];
             
-            memorytable.appendChild(brickgrid);
-            brickgrid.appendChild(link);
-            brickgrid.appendChild(memorybricks[i])
-            link.appendChild(picture);
+            link.setAttribute("class", "memorybrick");
             
+            div.appendChild(link);
+            link.appendchild(arrayTest);
             // onclick funtion goes here
         }
 
-    }
+    },
 };
 
 window.onload = Memory.init;  //init run when site is loaded
