4225e76a16e1d521f16b6a4469953ed1b1cfcf2e
Moa Alfredsson <moa.alfredsson@gmail.com>
2014-12-05 00:48:50 +0000

    Working version.. I think

--- 3-gameon/memory/js/memory.js
+++ 3-gameon/memory/js/memory.js
@@ -2,9 +2,11 @@
 
 var Memory = {
     // define variables
-    memoryBricks: [],
-    turnedBricks: [],
+  	memoryBricks: [],
+    pairedBricks: new Array(),
     lastBrick: undefined,
+    isTurning: false,
+    counter: 0,
     
     init: function(){
         Memory.memoryBricks = RandomGenerator.getPictureArray(4,4);
@@ -16,8 +18,8 @@ var Memory = {
         
         function newBrick(i){
              // create new link and image
-            var div = document.getElementById("memoryboard");
-            var link = document.createElement("a"),
+            var div = document.getElementById("memoryboard"),
+            link = document.createElement("a"),
             image = document.createElement("img");
             
             link.setAttribute("class", "memorybrick");
@@ -34,26 +36,53 @@ var Memory = {
                 return false;
             };
         }
+      
+        function contains(a, obj) {
+            for (var i = 0; i < a.length; i++) {
+                if (a[i] === obj) {
+                    return true;
+                }
+            }
+            return false;
+        }
                     
         //function for turning bricks
         function turnBrick(brickID){
+            if(contains(Memory.pairedBricks, brickID) || Memory.isTurning === true) {
+            	return;
+            }
+          
             // turn chosen brick, assign image
             var currentBrickImage = document.getElementById(brickID);
-            var lastBrickImage = document.getElementById(brickID);
+            var lastBrickImage = document.getElementById(Memory.lastBrick);
             currentBrickImage.setAttribute("src", "pics/" + Memory.memoryBricks[brickID] + ".png");
-          
-            if(lastBrick !== undefined) {
-            	lastBrick = brickID;
-            } else if (Memory.memoryBricks[lastBrick] === Memory.memoryBricks[brickID]) {
-            	// MATCH, DO NOTHING?
-                lastBrick = undefined;
+
+            if(Memory.lastBrick === undefined) {
+            	Memory.lastBrick = brickID;
+            	//add last brick to array
+            } else if (Memory.memoryBricks[Memory.lastBrick] === Memory.memoryBricks[brickID]) {
+            	// matched a pair
+                Memory.lastBrick = undefined;
+                // add pair to array
+                Memory.pairedBricks.push(brickID);
+                Memory.pairedBricks.push(Memory.lastBrick);
+                Memory.counter++;
+                
             } else {
-             	// NON MATCH
-                /*
-                currentBrickImage.setAttribute("src", "pics/0.png");
-                lastBrickImage.setAttribute("src", "pics/0.png");
-                */
-                lastBrick = undefined;
+             	// doesn't match, turn back bricks
+              	Memory.isTurning = true;
+                setTimeout(function(){
+                  	Memory.isTurning = false;
+                    currentBrickImage.setAttribute("src", "pics/0.png");
+                    lastBrickImage.setAttribute("src", "pics/0.png");
+                }, 1000);
+                
+                Memory.lastBrick = undefined;
+                Memory.counter++;
+            }
+            
+            if (Memory.pairedBricks.length === 16){
+                alert("Congratulations! You won with " + Memory.counter + " guesses!");
             }
         }
 
